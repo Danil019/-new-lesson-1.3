4264245ac44deda4d0810511038ebfd25a188d97
Danil019 <100717092+Danil019@users.noreply.github.com>
2024-09-05 13:53:26 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,10 @@
+name = 'Danil'
+print('Name:',name)
+age = 21
+print('Age:',age)
+age += 1
+print('New age:',age)
+# age = age + 4
+# print('Another year:', age)
+is_student = True
+print('Is student:',is_student)
